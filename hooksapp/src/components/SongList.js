c35b2ccddd1d68fc3cbf7703742f97eeae5d050a
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-25 21:43:47 +0500

    Lesson 12:useEffect() hook

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import uuid from 'uuid/dist/v1';
 import NewSongForm from './NewSongForm';
 
@@ -8,6 +8,13 @@ const SongList = () => {
         { title:'Let her go', id:'2'},
         { title:'Closer', id:'3'},
     ]);
+    const [age, setAge] = useState(15);
+    useEffect(()=>{
+        console.log('useEffect() run. Songs added')
+    },[songs])
+    useEffect(()=>{
+        console.log('useEffect() run. Age added')
+    },[age])
     const addSong = (title) => {
         setSongs([...songs, { title:title, id:uuid() } ])
     }
@@ -21,6 +28,7 @@ const SongList = () => {
                 }
             </ul>
             <NewSongForm addSong={addSong} />
+            <button onClick={ () => setAge(age+1) }>Add 1 to age:{age}</button>
         </div>      
     )
 }

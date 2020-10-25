cdb0c986da5f20ff6aca33c6220943f67b030c2a
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-25 12:20:46 +0500

    Lesson 3: Added theme context

---
+++ hookup/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ThemeContextProvider from './contexts/ThemeContext';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;

e2dfd971e0101eeba9b4f2fcfc77c616a25942bd
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-25 14:59:58 +0500

    Lesson 9:Just started using hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = ()=> {
+    return(
+        <div className="song-list">
+            <ul>
+                <li>Ali ya Maula</li>
+                <li>Hussain Labbaik</li>
+            </ul>
+        </div>
+    )
+}
+
+export default SongList;

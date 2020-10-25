8a0780fc06b3006c5d27ecdfab1d2cef0cf5a156
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-25 16:38:51 +0500

    Lesson 10:useState() hook

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import uuid from 'uuid/dist/v1';
 
-const SongList = ()=> {
+const SongList = () => {
+    const [songs, setsongs] = useState([
+        { title:'Hym for the weekend', id:'1'},
+        { title:'Let her go', id:'2'},
+        { title:'Closer', id:'3'},
+    ]);
+    const addSong = () => {
+        setsongs([...songs, { title:'New song added', id:uuid() } ])
+    }
     return(
         <div className="song-list">
             <ul>
-                <li>Ali ya Maula</li>
-                <li>Hussain Labbaik</li>
+                {
+                    songs.map(song =>{ 
+                        return(<li key={song.id}>{song.title}</li>)
+                    })
+                }
             </ul>
-        </div>
+            <button onClick={addSong}>Add new song</button>
+        </div>      
     )
 }
 

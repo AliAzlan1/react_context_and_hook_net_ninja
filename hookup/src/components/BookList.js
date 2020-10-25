2fc297d644c708b0efc3f418629f04dadfd4c66a
AliAzlan1 <aliazlan2002@gmail.com>
2020-10-25 22:52:34 +0500

    Lesson15:Added a BookList Context in form of functional component using useState and then integerated it with BookList components

--- hookup/src/components/BookList.js
+++ hookup/src/components/BookList.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
+  const { books } = useContext(BookContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
   return(
     <div className="book-list" style={{ color:theme.syntax, background:theme.bg }}>
-      <ul style={{ color:theme.syntax, background:theme.ui }}>
-        <li>The way of kings</li>
-        <li>The name of the wind</li>
-        <li>The final empire</li>
+      <ul style={{ background:theme.ui }}>
+        {
+          books.map(book => {
+            return( <li key={book.id}>{book.title}</li> );
+          })
+        }
       </ul>
     </div>
   );
